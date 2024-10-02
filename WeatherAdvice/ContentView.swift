2ce558b300f1ee99d29a91808a3fd033792daff8
Aimee Hong <honglp@bc.edu>
2024-10-02 11:03:49 -0400

    created basic app functions

--- WeatherAdvice/ContentView.swift
+++ WeatherAdvice/ContentView.swift
@@ -8,14 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var imageName = ""
+    @State private var adviceMessage = ""
+    @State private var temp = ""
+    @FocusState private var isFocused: Bool
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Weather Advice")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.white)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(.teal)
+                
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .animation(.default, value: imageName)
+            
+            Text(adviceMessage)
+                .font(.largeTitle)
+                .frame(height: 80)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .animation(.default, value: adviceMessage)
+            
+            Spacer()
+            
+            HStack {
+                Text("What is the temp?")
+                    .font(.title)
+                
+                TextField("", text: $temp)
+                    .textFieldStyle(.roundedBorder)
+                    .frame(width: 65)
+                    .onTapGesture {
+                        isFocused = true
+                    }
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(.gray, lineWidth: 2)
+                    }
+                    .keyboardType(.numberPad)
+            }
+            .font(.title)
+            
+            Button("Get Advice") {
+                giveAdvice()
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.teal)
+            .font(.title2)
+            
         }
         .padding()
+    }
+    
+    func giveAdvice() {
+        guard let temp = Int(temp) else {
+            adviceMessage = "Please enter a valid temperature"
+            return
+        }
+        
+        switch temp {
+        case 76...:
+            adviceMessage = "It's Hot"
+            imageName = "hot"
+        case 63..<76:
+            adviceMessage = "Nice and mild"
+            imageName = "warm"
+        case 45..<63:
+            adviceMessage = "You're going to need a sweater"
+            imageName = "cool"
+        case 33..<45:
+            adviceMessage = "You're going to need a coat"
+            imageName = "cold"
+        default:
+            adviceMessage = "Bundle up, it's freezing!"
+            imageName = "freezing"
+        }
     }
 }
 
